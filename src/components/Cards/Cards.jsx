f274180adc1b303bd8b5597e738d54c1d04afb92
Cal9233 <calvin.m71@hotmail.com>
2020-08-21 11:26:27 -0400

    master added

---
+++ src/components/Cards/Cards.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { Cards, CardContent, Typography, Grid } from "@material-ui/core";
+
+const Cards = (props) => {
+  console.log(props);
+
+  return <h1>Cards</h1>;
+};
+
+export default Cards;
